f8f8e0f50fdfdcd1b2b7ad53ea88ff58c1e8fdc6
princegupta17 <princegupta444555@gmail.com>
2024-04-21 03:06:49 +0530

    added delete, toggle, update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
-import "./App.css";
 import { Todoprovider } from "./contexts";
+import Todoform from "./components/Todoform";
+import TodoItem from "./components/TodoItem";
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -48,9 +49,17 @@ function App() {
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">Todos</h1>
-          <div className="mb-4">{/* Todo form goes here */}</div>
+          <div className="mb-4">
+            {/* Todo form goes here */}
+            <Todoform />
+          </div>
           <div className="flex flex-wrap gap-y-3">
             {/*loop and add todoitem here */}
+            {todos.map((todo) => (
+              <div key={todo.id} className="w-full">
+                <TodoItem todo={todo} />
+              </div>
+            ))}
           </div>
         </div>
       </div>
